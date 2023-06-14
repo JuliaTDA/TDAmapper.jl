c0cd940ae820f7bf6d1f0507e3fb1947c4fbc49e
G. Vituri <56522687+vituri@users.noreply.github.com>
2023-06-14 03:27:24 -0300

    quinquagésima tentativa

--- docs/make.jl
+++ docs/make.jl
@@ -1,3 +1,11 @@
+# Inside make.jl
+push!(LOAD_PATH,"../src/")
+
+import Pkg; Pkg.add("Documenter");
 using Documenter, TDAmapper
 
 makedocs(sitename="My Documentation")
+
+deploydocs(
+    repo = "github.com/vituri/TDAmapper.jl.git",
+)
--- src/TDAmapper.jl
+++ src/TDAmapper.jl
@@ -1,17 +1,17 @@
 module TDAmapper
 
-using BenchmarkTools; using Distances; using ProgressMeter;
-import Base.Threads.@threads
+# using BenchmarkTools; using Distances; using ProgressMeter;
+# import Base.Threads.@threads
 
 export unique_sort,
     epsilon_net
 
-include("types.jl");
-include("sampling.jl");
-include("filter.jl");
-include("covering.jl");
-include("clustering.jl");
-include("plots.jl");
+# include("types.jl");
+# include("sampling.jl");
+# include("filter.jl");
+# include("covering.jl");
+# include("clustering.jl");
+# include("plots.jl");
 include("utils.jl");
 
 end # module
