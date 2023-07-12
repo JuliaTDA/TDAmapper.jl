b94296e76db9081bff05e7f3006c35f357c2f5c7
G. Vituri <56522687+vituri@users.noreply.github.com>
2023-07-12 01:40:47 -0300

    import graph

--- src/mapper.jl
+++ src/mapper.jl
@@ -47,11 +47,20 @@
 # using ..TDAmapper
 
 """
-    mapper(X, filter_values, covering_intervals, clustering)
+mapper(
+    X::PointCloud
+    ,filter_values::Vector{<:Real}
+    ,covering_intervals::Vector{<:Interval}
+    ;clustering = cluster_dbscan
+    )
 
 Create the mapper graph of a given pointcloud `X`,
 with a vector of `filter_values`, using the
 `covering_intervals` and a `clustering` function.
+
+# Examples
+
+etc.
 """
 function mapper(
     X::PointCloud
@@ -69,7 +78,7 @@ function mapper(
     # create the mapper graph
     adj_matrix = adj_matrix_from_pb(clustered_pb_ids)
     
-    mapper_graph = Graph(adj_matrix)
+    mapper_graph = Graphs.Graph(adj_matrix)
 
     mapper = Mapper(
         X = X
